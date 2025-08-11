bb4f3c4d8eb4406944fe047944e97b81163d75da
PlusMinusAns <kimjaehyun9605@gmail.com>
2025-08-11 21:49:11 +0900

    pipeline

--- keras2/keras78_gradient1.py
+++ keras2/keras78_gradient1.py
@@ -1,1 +1,15 @@
-gradient1 = lambda x : 2*x - 4
+import numpy as np
+import matplotlib.pyplot as plt
+
+f = lambda x : x**2 -4*x +6
+x = np.linspace(-1,6,100)
+print(x, len(x))
+
+y = f(x)
+
+plt.plot(x, y, 'k-')
+plt.plot(2,2,'sk', color='red')
+plt.grid()
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
